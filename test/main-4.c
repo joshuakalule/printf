c5a6db06a223cc1efe3fe2b2d81c4d0ffd5761e7
Kalule Joshua <kaljoshkalule@gmail.com>
2023-09-21 21:39:41 +0000

    resolve issues with decimal printf

--- test/main-4.c
+++ test/main-4.c
@@ -9,7 +9,16 @@
  */
 int main(void)
 {
-	_printf("%K\n");
+	int len, len2;
 
+	len = _printf("iddi%diddiiddi\n", 1024);
+	len2 = printf("iddi%diddiiddi\n", 1024);
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	return (0);
 }
